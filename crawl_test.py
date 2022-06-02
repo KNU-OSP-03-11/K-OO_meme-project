ccc9e054da2f7ad1d82b87caa4ed59833e6e5985
DWK3732 <ugi9297@naver.com>
2022-06-03 04:19:50 +0900

    crawler updated with mysql

--- crawl_test.py
+++ crawl_test.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+from mimetypes import init
 import re
 import requests
 from bs4 import BeautifulSoup
-import pymysql
 from konlpy.tag import Okt
+from DB_Functions import *
 okt = Okt() # 한글 명사분석
 import time # 시간분석
 start = time.time()
 
+ 
 # process for collecting only words
 def process( soup_selction_list ):
     global DATA_dic
@@ -23,7 +25,7 @@ def process( soup_selction_list ):
         # a-zA-Z^ \u3131-\u3163\uac00-\ud7a3
         # sentence = list(content.get_text().split())
         for word in sentence:
-            if flag == 1:
+            if flag == 1: # if K + ' ' + word
                 word = ' '.join(["K",word])
                 if DATA_dic.get(word)==None:
                     DATA_dic[word] = 1
@@ -32,7 +34,7 @@ def process( soup_selction_list ):
                     DATA_dic[word] += 1
                 flag = 0
                 break
-            if word.find("K") == 0 :
+            if word.find("K") == 0 : # found K-word
                 noun = okt.nouns(word)
                 if noun!=[]:
                     if word.find("-") == 1:
@@ -41,7 +43,7 @@ def process( soup_selction_list ):
                     else:
                         noun.insert(0,"K")
                         word = ''.join(elements for elements in noun)
-                if len(word)==1 :
+                if len(word)==1 : # if K + ' ' + word
                     flag = 1
                     continue
                 if DATA_dic.get(word)==None:
@@ -51,10 +53,9 @@ def process( soup_selction_list ):
                     DATA_dic[word] += 1
                 break
 
-
 url_list = []
 url_list.append(u'https://search.naver.com/search.naver?where=news&query=K&sm=tab_opt&sort=0&photo=0&field=0&pd=0&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3Aall&is_sug_officeid=0')
-fp = open("output.txt",'w') # 확인용
+# fp = open("output.txt",'w') # 확인용
 for i in range(1,100):
     url_list.append(u"https://search.naver.com/search.naver?where=news&sm=tab_pge&query=K&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=18&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={}1".format(i))
 # DATA_dic as dictionary of { "word" : "url" }
@@ -74,8 +75,10 @@ words = list(DATA_dic.keys())
 frequencies = list(DATA_dic.values())
 title_s = list(DATA_T_dic.values())
 for i in range(len(words)):
-    print("{} {} {}".format(words[i],frequencies[i],title_s[i]),file=fp) # 확인용
-print("time : ",time.time() - start,file=fp)
-fp.close()# 확인용
-
-    
+    addMeme(initMeme(words[i],title_s[i],frequencies[i],1))
+    # print("{} {} {}".format(words[i],frequencies[i],title_s[i]),file=fp) # 확인용
+#print("time : ",time.time() - start,file=fp)
+#fp.close()# 확인용
+#ranking = rankFreq(10,0)    
+#for i in ranking:
+#    print(i)
