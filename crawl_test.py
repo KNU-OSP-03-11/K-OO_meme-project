bbb508949222ea57940336050be19a58558d2a14
DWK3732 <ugi9297@naver.com>
2022-06-08 00:29:51 -0700

    updated crawler

--- crawl_test.py
+++ crawl_test.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-from mimetypes import init
 import re
 import requests
 from bs4 import BeautifulSoup
 from konlpy.tag import Okt
-from DB_Functions import *
+from DB_Functions import * # mysql functions
 okt = Okt() # 한글 명사분석
-import time # 시간분석
-start = time.time()
-
  
 # process for collecting only words
 def process( soup_selction_list ):
@@ -18,6 +14,7 @@ def process( soup_selction_list ):
     global DATA_T_dic
     flag = 0
     for content in soup_selction_list:
+        link = content.attrs['href']
         title = content.get_text()
         sentence = re.findall(r"(?i)\b[a-zA-Z\-\u3131-\u3163\uac00-\ud7a3]+\b", title)
         #r"(?i)\b[a-z]+\b", content.get_text()
@@ -26,10 +23,11 @@ def process( soup_selction_list ):
         # sentence = list(content.get_text().split())
         for word in sentence:
             if flag == 1: # if K + ' ' + word
-                word = ' '.join(["K",word])
+                word = ' '.join(["K-",word])
                 if DATA_dic.get(word)==None:
                     DATA_dic[word] = 1
                     DATA_T_dic[word] = " ".join(sentence)
+                    DATA_U_dic[word] = link
                 else :
                     DATA_dic[word] += 1
                 flag = 0
@@ -41,7 +39,7 @@ def process( soup_selction_list ):
                         noun.insert(0,"K-")
                         word = ''.join(elements for elements in noun)
                     else:
-                        noun.insert(0,"K")
+                        noun.insert(0,"K-")
                         word = ''.join(elements for elements in noun)
                 if len(word)==1 : # if K + ' ' + word
                     flag = 1
@@ -49,21 +47,21 @@ def process( soup_selction_list ):
                 if DATA_dic.get(word)==None:
                     DATA_dic[word] = 1
                     DATA_T_dic[word] = " ".join(sentence)
+                    DATA_U_dic[word] = link
                 else :
                     DATA_dic[word] += 1
                 break
 
 url_list = []
 url_list.append(u'https://search.naver.com/search.naver?where=news&query=K&sm=tab_opt&sort=0&photo=0&field=0&pd=0&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3Aall&is_sug_officeid=0')
-# fp = open("output.txt",'w') # 확인용
-for i in range(1,100):
+for i in range(1,10):
     url_list.append(u"https://search.naver.com/search.naver?where=news&sm=tab_pge&query=K&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=18&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={}1".format(i))
-# DATA_dic as dictionary of { "word" : "url" }
 global DATA_dic
 DATA_dic = {}
-# DATA_T_dic as dictionary of { "word" : "title" }
 global DATA_T_dic
 DATA_T_dic = {}
+global DATA_U_dic
+DATA_U_dic = {}
 for url in url_list:
     res = requests.get(url)
     # implement BeautifulSoup
@@ -74,11 +72,6 @@ for url in url_list:
 words = list(DATA_dic.keys())
 frequencies = list(DATA_dic.values())
 title_s = list(DATA_T_dic.values())
+links = list(DATA_U_dic.values())
 for i in range(len(words)):
-    addMeme(initMeme(words[i],title_s[i],frequencies[i],1))
-    # print("{} {} {}".format(words[i],frequencies[i],title_s[i]),file=fp) # 확인용
-#print("time : ",time.time() - start,file=fp)
-#fp.close()# 확인용
-#ranking = rankFreq(10,0)    
-#for i in ranking:
-#    print(i)
+    addMeme(initMeme(words[i],title_s[i],links[i],frequencies[i],0))
